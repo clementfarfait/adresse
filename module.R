19aad43b1b695aee5062deaa2c03e3fa8bb1a1d7
Clément Farfait <72358367+clementfarfait@users.noreply.github.com>
2022-05-19 08:56:08 +0200

    Add files via upload

--- module.R
+++ module.R
@@ -1,10 +1,16 @@
 version = read.table("version.txt")
 version = version$V1[1]
 
+options(shiny.maxRequestSize = 30*1024^2)
+library(stringr)
+library(readxl)
+library(writexl)
+library(crayon)
+
 ##### Code client
 
 ui <- fluidPage(
-    titlePanel(paste0("Correction d'adresse - "),version),
+    titlePanel(paste0("Correction d'adresse - ",version)),
 
     sidebarLayout(
         sidebarPanel(
@@ -20,45 +26,53 @@ ui <- fluidPage(
 ##### Code serveur
 
 server <- function(input, output) {
+    
     output$contents <- renderTable({
         inFile <- input$file1
         
         if (is.null(inFile))
             return(NULL)
         
-        text <- input$input1
-        new_text <- as.data.frame(str_split(text, ";"))
+        po = 0
         
-        replace <- read.table("https://raw.githubusercontent.com/clementfarfait/clementfarfait.github.io/main/adresse", sep=";", header=TRUE)
-        for(i in 1:length(replace$adresse)){
-            replace[i,1] <- str_replace(replace[i,1], pattern = "apostrophe", replacement = "'")
-        }
-        for(i in 1:length(replace$correction)){
-            replace[i,2] <- str_replace(replace[i,2], pattern = "vide", replacement = " ")
-        }
-        cat(replace)
-        
-        for(i in 1:length(new_text[,1])){
-            sep <- as.data.frame(str_split(new_text[i,1], ":"))
-            data <- read_excel(inFile$datapath, sheet = sep[1,1])
+        withProgress(message = 'Modification en cours..', detail = paste0("[",po,"%]"), value = 0, {
+            text <- input$input1
+            new_text <- as.data.frame(str_split(text, ";"))
             
-            for(z in 1:length(colnames(data))){
-                if(colnames(data[z]) == sep[2,1]){
-                    indice = z
-                }
+            replace <- read.table("https://raw.githubusercontent.com/clementfarfait/adresse/main/modifications", sep=";", header=TRUE)
+            for(i in 1:length(replace$adresse)){
+                replace[i,1] <- str_replace(replace[i,1], pattern = "apostrophe", replacement = "'")
+            }
+            for(i in 1:length(replace$correction)){
+                replace[i,2] <- str_replace(replace[i,2], pattern = "vide", replacement = " ")
             }
             
-            for(p in 1:nrow(data[,indice])){
-                data[p,indice] <- toupper(data[p,indice])
-                for(y in 1:length(replace$adresse)){
-                    data[p,indice] <- str_replace(data[p,indice], pattern = replace$adresse[y], replacement = replace$correction[y])
+            for(i in 1:length(new_text[,1])){
+                sep <- as.data.frame(str_split(new_text[i,1], ":"))
+                data <- read_excel(inFile$datapath, sheet = sep[1,1])
+                
+                for(z in 1:length(colnames(data))){
+                    if(colnames(data[z]) == sep[2,1]){
+                        indice = z
+                    }
                 }
+                
+                n = nrow(data[,indice])
+                
+                for(p in 1:nrow(data[,indice])){
+                    data[p,indice] <- toupper(data[p,indice])
+                    for(y in 1:length(replace$adresse)){
+                        data[p,indice] <- str_replace(data[p,indice], pattern = replace$adresse[y], replacement = replace$correction[y])
+                    }
+                    setProgress(value = p/n, detail = paste0("[",round(p/n*100,1),"%]"))
+                    #cat(paste0("\n[",green("OK"),"] Ligne ",p))
+                }
+                
+                new_data <- write_xlsx(data[,indice], paste0("../modifications/",sep[1,1],"-",sep[2,1],".xlsx",sep=""))
+                cat(paste0("\nOK: modifications/",sep[1,1],"-",sep[2,1],".xlsx\n"))
             }
-            
-            new_data <- write_xlsx(data[,indice], paste0("../modifications/",sep[1,1],"-",sep[2,1],".xlsx",sep=""))
-            cat(paste0("OK: modifications/",sep[1,1],"-",sep[2,1],".xlsx"))
-        }
-        cat("OK!")
+        })
+        print("Modifications terminées.")
     })
     
     output$value <- renderText({ input$caption })
